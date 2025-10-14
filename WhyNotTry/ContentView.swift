013db67eb4a0c6e2e20f3d0d5e245b63aee9b5fd
JanSplat2 <jan.splat@outlook.de>
2025-10-14 15:19:37 -0600

    MVP and therefore is everything done :)

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,15 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling"]
+    
+    @State var selected = "Archery"
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint]
+                           
+    @State private var id = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            Circle()
+                .fill(colors.randomElement() ?? .blue)
+                .padding()
+                .overlay(
+                    Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+            Text("\(selected)!")
+                .font(.title)
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
+        .transition(.slide)
+        .id(id)
     }
+    
 }
 
 #Preview {
