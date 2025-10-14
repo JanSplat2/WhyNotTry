f369cb6df18689d36403805d039745341b08dd1f
JanSplat2 <jan.splat@outlook.de>
2025-10-14 15:07:30 -0600

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Dittrich, Jan - Student on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
